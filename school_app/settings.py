39e8a3ca39e643d0e40da58be2ef07286b856d33
Sherif <adigunsherif@gmail.com>
2020-05-06 13:40:07 +0100

    settingsupdated

--- school_app/settings.py
+++ school_app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://django-school-app.herokuapp.com/']
+ALLOWED_HOSTS = ['https://django-school-app.herokuapp.com']
 
 
 # Application definition
