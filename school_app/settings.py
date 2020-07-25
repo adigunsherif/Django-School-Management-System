d5f1b9e4959828806d7fbe824452d80b33f6f9b4
Sherif Adigun <30326604+adigunsherif@users.noreply.github.com>
2020-07-25 16:16:29 +0100

    Update settings.py

--- school_app/settings.py
+++ school_app/settings.py
@@ -28,7 +28,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-school-app.herokuapp.com']
 
