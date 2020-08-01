62bbf289ee8aeb77083bd858a35a4e5c7ee35f8d
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2020-08-01 16:01:40 +0530

    Phone number validation under staff app. Found missing.

--- staffs/models.py
+++ staffs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from django.core.validators import RegexValidator
 
 class Staff(models.Model):
   STATUS = [
@@ -13,17 +14,19 @@ class Staff(models.Model):
       ('female', 'Female')
   ]
 
-  current_status = models.CharField(
-      max_length=10, choices=STATUS, default='active')
-  surname = models.CharField(max_length=200)
-  firstname = models.CharField(max_length=200)
-  other_name = models.CharField(max_length=200, blank=True)
-  gender = models.CharField(max_length=10, choices=GENDER, default='male')
-  date_of_birth = models.DateField(default=timezone.now)
-  date_of_admission = models.DateField(default=timezone.now)
-  mobile_number = models.CharField(max_length=15, blank=True)
-  address = models.TextField(blank=True)
-  others = models.TextField(blank=True)
+  current_status      = models.CharField(max_length=10, choices=STATUS, default='active')
+  surname             = models.CharField(max_length=200)
+  firstname           = models.CharField(max_length=200)
+  other_name          = models.CharField(max_length=200, blank=True)
+  gender              = models.CharField(max_length=10, choices=GENDER, default='male')
+  date_of_birth       = models.DateField(default=timezone.now)
+  date_of_admission   = models.DateField(default=timezone.now)
+
+  mobile_num_regex    = RegexValidator(regex="^[0-9]{10,15}$", message="Entered mobile number isn't in a right format!")
+  mobile_number       = models.CharField(validators=[mobile_num_regex], max_length=13, blank=True)
+  
+  address             = models.TextField(blank=True)
+  others              = models.TextField(blank=True)
 
   def __str__(self):
     return f'{self.surname} {self.firstname} {self.other_name}'
