9e08304606bf5a827f6bfbf38bea394fa0897d07
Sharif <orzikulov@gmail.com>
2023-02-02 17:15:25 +0300

    db changed to Postgres 15

--- hrm_app/settings.py
+++ hrm_app/settings.py
@@ -87,11 +87,22 @@ WSGI_APPLICATION = "hrm_app.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tutorial',
+        'USER': 'postgres',
+        'PASSWORD': 'admin777',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#    }
+#}
 
 
 # Password validation
