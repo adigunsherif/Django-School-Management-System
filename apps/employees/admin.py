898f54e5e0ef1cb76494a4145e2489455f4ca553
Sharif <orzikulov@gmail.com>
2023-02-03 12:29:57 +0300

    admin site added

--- apps/employees/admin.py
+++ apps/employees/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Employee
+
+
+#admin.site = MyAdminSite(name='myadmin')
+admin.site.register(Employee)
+
+admin.site.site_header = "МиграДок Admin"
+admin.site.site_title = "МиграДок Admin Portal"
+admin.site.index_title = "Welcome to МиграДок Portal"
+
--- hrm_app/urls.py
+++ hrm_app/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("apps.corecode.urls")),
+    path('admin/', admin.site.urls),
 #    path("student/", include("apps.students.urls")),
 #    path("staff/", include("apps.staffs.urls")),
 #    path("finance/", include("apps.finance.urls")),
