223a1db024794bad6ff92f0836e7516c63495571
Sharif <orzikulov@gmail.com>
2023-02-03 13:12:09 +0300

    Debug - false

--- hrm_app/settings.py
+++ hrm_app/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "__$1ud47e&nyso5h5o3fwnqu4+hfqcply9h$k*h2s34)hn5@nc"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
