e2e64ccbe9107d0351df9ee1563ac3f37008f455
Sharif <orzikulov@gmail.com>
2023-02-03 14:54:53 +0300

    Changes

--- hrm_app/settings.py
+++ hrm_app/settings.py
@@ -165,9 +165,8 @@ if DEBUG:
             os.path.join(BASE_DIR, 'static')
        ]
 else:
-        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+        STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
