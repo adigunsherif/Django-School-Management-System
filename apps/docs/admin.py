f80dd6f5bb3c9e7c8f3bc8c0df1865591ada154f
Sharif <orzikulov@gmail.com>
2023-02-09 18:43:50 +0300

    Changes

--- apps/docs/admin.py
+++ apps/docs/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Doc
+
+
+#admin.site = MyAdminSite(name='myadmin')
+admin.site.register(Doc)
+
+admin.site.site_header = "МигрантДок Admin"
+admin.site.site_title = "МигрантДок Admin Portal"
+admin.site.index_title = "Welcome to МигрантДок Portal"
+
