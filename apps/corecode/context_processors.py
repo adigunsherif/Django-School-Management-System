89ddbf08de31ee71b4600c91bd17ffffd06929bc
Sharif <orzikulov@gmail.com>
2023-02-22 15:11:00 +0300

    Changes dashboard

--- apps/corecode/context_processors.py
+++ apps/corecode/context_processors.py
@@ -1,3 +1,5 @@
+from django.db.models import F
+
 from .models import AcademicSession, AcademicTerm, SiteConfig
 from apps.employees.models import Employee
 
@@ -6,11 +8,11 @@ def site_defaults(request):
     current_session = AcademicSession.objects.get(current=True)
     current_term = AcademicTerm.objects.get(current=True)
     vals = SiteConfig.objects.all()
-    employee_count = Employee.objects.count()
+    active_employee_count = Employee.objects.filter(current_status='active').count() # Get all employees with status 'active'
     contexts = {
         "current_session": current_session.name,
         "current_term": current_term.name,
-        "employee_count": employee_count,
+        "active_employee_count": active_employee_count,
     }
     for val in vals:
         contexts[val.key] = val.value
