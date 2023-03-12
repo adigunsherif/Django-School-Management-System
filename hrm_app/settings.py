1523c1cb505590fd3e11dd00544e422f53cf1ed6
Sharif Orz <orzikulov@gmail.com>
2023-03-12 14:21:47 +0300

    Changes

--- hrm_app/settings.py
+++ hrm_app/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'WIafGf7vc5U39no3E10S',
+        'PASSWORD': '2wL7KYKTsfLEWodBKrpG',
         'HOST': 'containers-us-west-154.railway.app',
         'PORT': '7371',
     }
